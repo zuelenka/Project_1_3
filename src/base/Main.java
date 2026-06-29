50cd16681124c575a5b1df9e14c299b1dc14a111
zuele <zuelenka@gmail.com>
2026-06-29 16:34:33 +0300

    Initial commit

---
+++ src/base/Main.java
@@ -0,0 +1,19 @@
+package base;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Elena");
+        System.out.println(123 * 456);
+        System.out.println("*".repeat(10));
+        System.out.print("Раз ");
+        System.out.print("два ");
+        System.out.print("три ");
+        System.out.print("четыре ");
+        System.out.print("пять ");
+        System.out.print("Java ");
+        System.out.print("буду ");
+        System.out.print("знать ");
+        System.out.print("на ");
+        System.out.print("пять!");
+    }
+}
